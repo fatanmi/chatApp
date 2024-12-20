77de62b5169fe130b9c9ebb94b8f0ea83656640f
Ebenezer Fatanmi <originalbami@gmail.com>
2024-12-20 01:14:52 +0000

    reworked css

--- src/Components/list/chatList/ChatList.jsx
+++ src/Components/list/chatList/ChatList.jsx
@@ -4,12 +4,14 @@ import "./chatList.css";
 function ChatList() {
   return (
     <div>
-      <div className="searchBar">
+      <div className="chatList">
         <div className="search">
-          <img src="/search.png" alt="" />
-          <input type="text" name="" id="" placeholder="Search" />
+          <div className="searchBar">
+            <img src="/search.png" alt="" />
+            <input type="text" name="" id="" placeholder="Search" />
+          </div>
+          <img src="/plus.png" alt="" />
         </div>
-        <img src="/plus.png" alt="" />
       </div>
     </div>
   );
